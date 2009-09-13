337acf126eb2b2b5113c04e38595555a2de7035f
Jakub Vysoky <jakub@borka.cz>
2009-09-13 11:35:35 -0700

    mock datetime

--- pythonrc.py
+++ pythonrc.py
@@ -4,13 +4,12 @@ from os.path import dirname, join
 base = dirname(__file__)
 
 # check whether you are in virtualenv
-print 'virtual environment enabled :: takze dobry vecer...'
+print 'virtual environment enabled with mocked datetime'
 
 # activate virtualenv
 activate_this = join(base, 'bin', 'activate_this.py')
 execfile(activate_this, dict(__file__=activate_this))
 
-'''
 # mock up datetime
 import datetime
 old_datetime = datetime.datetime
@@ -19,5 +18,4 @@ class NewDatetime(datetime.datetime):
     def now(cls):
         return cls(1983, 6, 4)
 datetime.datetime = NewDatetime
-'''
 
