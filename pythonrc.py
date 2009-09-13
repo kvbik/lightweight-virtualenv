f318f3acf14c3fc5e63736d5f0010ad02abfdcb9
Jakub Vysoky <jakub@borka.cz>
2009-09-13 10:24:29 -0700

    you can mock datetime very easily with pythonrc

--- pythonrc.py
+++ pythonrc.py
@@ -10,3 +10,14 @@ print 'virtual environment enabled :: takze dobry vecer...'
 activate_this = join(base, 'bin', 'activate_this.py')
 execfile(activate_this, dict(__file__=activate_this))
 
+'''
+# mock up datetime
+import datetime
+old_datetime = datetime.datetime
+class NewDatetime(datetime.datetime):
+    @classmethod
+    def now(cls):
+        return cls(1983, 6, 4)
+datetime.datetime = NewDatetime
+'''
+
