59e4df690405a9c7815da7c4ffabf134032f7f9b
Jakub Vysoky <jakub@borka.cz>
2009-09-14 16:27:00 -0700

    mock more time functions

--- pythonrc.py
+++ pythonrc.py
@@ -11,11 +11,21 @@ activate_this = join(base, 'bin', 'activate_this.py')
 execfile(activate_this, dict(__file__=activate_this))
 
 # mock up datetime
-import datetime
-old_datetime = datetime.datetime
+import datetime, time
+mytime = 423592200 # 1983/6/4 0:30 CEST
+mydatetime = datetime.datetime.fromtimestamp(mytime)
+old_localtime = time.localtime
 class NewDatetime(datetime.datetime):
     @classmethod
     def now(cls):
-        return cls(1983, 6, 4)
+        return mydatetime
+def new_time():
+    return mytime
+def new_localtime(seconds=None):
+    if not seconds:
+        return mydatetime.timetuple()
+    return old_localtime(seconds)
 datetime.datetime = NewDatetime
+time.time = new_time
+time.localtime = new_localtime
 
