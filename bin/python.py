de6216ebadc2ebdaffd1423fc197ebac3f69cf91
Jakub Vysoky <jakub@borka.cz>
2009-09-29 13:07:02 +0200

    use os.system instead of subprocess.call
    because of better handling KeyboardInterrupt

--- bin/python.py
+++ bin/python.py
@@ -9,13 +9,34 @@ import os, sys
 from os.path import join, dirname, pardir, abspath
 import subprocess
 
-base = dirname(__file__)
-pypath = os.environ.get('PYTHONPATH', '').split(':')
-os.environ['PYTHONPATH'] = ':'.join([abspath(join(base, pardir))] + pypath)
+def get_this_path():
+    base = dirname(__file__)
+    thispath = abspath(join(base, pardir))
+    return thispath
 
-cmd = ['python'] + sys.argv[1:]
-try:
-    subprocess.call(cmd)
-except KeyboardInterrupt:
-    pass
+def inject_pythonpath():
+    '''
+    insert current path into pythonpath
+    '''
+    pypath = os.environ.get('PYTHONPATH', '').split(':')
+    thispath = get_this_path()
+    try:
+        pypath.remove('')
+        pypath.remove(thispath)
+    except ValueError:
+        pass
+    pypath.insert(0, thispath)
+    os.environ['PYTHONPATH'] = ':'.join(pypath)
+
+def prepare_command():
+    '''
+    prepare command to run
+    '''
+    cmd = ['python'] + sys.argv[1:]
+    cmd = map(lambda s: '"%s"' % s.replace('"', '\\"'), cmd)
+    cmdstr = ' '.join(cmd)
+    return cmdstr
+
+inject_pythonpath()
+os.system(prepare_command())
 
