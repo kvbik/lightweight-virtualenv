7e93da3578e5345c1fabbbf0a76d3d2efcf676dc
Jakub Vysoky <jakub@borka.cz>
2010-01-25 22:07:41 +0100

    create proper lib/site-packages subdir

--- pythonrc.py
+++ pythonrc.py
@@ -1,7 +1,14 @@
 
-from os.path import dirname, join
+import sys
+from os.path import dirname, join, exists
+from os import makedirs
 
 base = dirname(__file__)
+
+# create lib subdir
+site_packages = join(base, 'lib', 'python%s' % sys.version[:3], 'site-packages')
+if not exists(site_packages):
+    makedirs(site_packages)
 
 # check whether you are in virtualenv
 #print 'virtual environment enabled :: takze dobry vecer...'
