ec66c21c17cd81b3f66233fd186153019b88c192
Jakub Vysoky <jakub@borka.cz>
2010-02-11 19:44:30 +0100

    mock distutils spawn not to pass -E argument

--- pythonrc.py
+++ pythonrc.py
@@ -1,4 +1,5 @@
 
+import distutils.spawn
 from os.path import dirname, join, exists
 import distutils.command.install
 
@@ -18,6 +19,17 @@ SCHEME = {
 schemes = distutils.command.install.INSTALL_SCHEMES
 for k in schemes.keys():
     schemes[k] = SCHEME
+
+# do not spawn with -E argument
+def spawn (cmd,
+           search_path=1,
+           verbose=0,
+           dry_run=0):
+    if '-E' in cmd:
+        cmd.remove('-E')
+    distutils.spawn._spawn(cmd, search_path, verbose, dry_run)
+distutils.spawn._spawn = distutils.spawn.spawn
+distutils.spawn.spawn = spawn
 
 # activate virtualenv
 activate_this = join(base, 'bin', 'activate_this.py')
